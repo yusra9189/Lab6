437d648adbdaa35ca18e7dbd2a0a6495a6da96cd
Your Name <[you@example.com])>
2023-10-22 22:17:21 -0400

    Adding encode function

---
+++ main.py
@@ -0,0 +1,31 @@
+def encode(x):
+    for i in x:
+        i = int(i) + 3
+        if i >= 10:
+            i = i - 10
+        print(i, end='')
+    return ''
+
+def decode(x):
+    pass
+
+
+while True:
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+    print('\n\n')
+
+    option = int(input("Please enter an option: "))
+    if option == 1:
+        password = input("Please enter your password to encode: ")
+    print("Your password has been encoded and stored!")
+    print()
+
+    if option == 2:
+        pass
+
+    if option == 3:
+        break
