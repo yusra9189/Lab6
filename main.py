a80528e4ad7febecaf73fca88fb1970e05446b38
jilliansanchezz <jilliansanchez@ufl.edu>
2023-10-24 19:52:20 -0400

    added decode function

--- main.py
+++ main.py
@@ -7,8 +7,14 @@ def encode(x):
         res += str(temp)
     return res
 
-def decode(x):
-    pass
+# Decoder Function
+def decode(encoded_password):
+    decoded_password = ""
+    for digit in encoded_password:
+        original_digit = str((int(digit) - 3) % 10)
+        decoded_password += original_digit
+    return decoded_password
+
 
 
 while True:
@@ -25,9 +31,12 @@ while True:
         encoded = encode(password)
     print("Your password has been encoded and stored!")
 
-
     if option == 2:
-        print(f"The encoded password is {encoded}")
+        if encoded:
+            decoded = decode(encoded)
+            print(f"The encoded password is {encoded}, and the original password is {decoded}")
+        else:
+            print("Please encode a password first.")
 
     if option == 3:
         break
